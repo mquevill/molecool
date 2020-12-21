e8e84ca4992df4c0ad4a2f28bd83be40c229dd81
Michael Quevillon <mquevill@nd.edu>
2020-12-21 12:57:21 -0500

    Add docs to measure functions

--- molecool/measure.py
+++ molecool/measure.py
@@ -6,8 +6,23 @@ Performs measurements of various properties
 import numpy as np
 
 def calculate_angle(rA, rB, rC, degrees=False):
-    # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
-    # by setting degrees=True
+    """
+    Calculate angle between three points.
+
+    Parameters
+    ----------
+    rA, rB, rC : np.ndarray
+        Coordinates of each point.
+
+    degrees : bool, Optional, default=False
+        Set whether or not to use degrees.
+
+    Returns
+    -------
+    theta : float
+        Angle between three points
+    """
+
     AB = rB - rA
     BC = rB - rC
     theta = np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
@@ -18,7 +33,27 @@ def calculate_angle(rA, rB, rC, degrees=False):
         return theta
 
 def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
+    """
+    Calculate distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        Coordinates of each point.
+
+    Returns
+    -------
+    dist : float
+        Distance between two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 1, 0])
+    >>> calculate_distance(r1, r2)
+    1.0
+    """
+
     d = rA - rB
     dist = np.linalg.norm(d)
     return dist
